329397b592eaa06bad926685e6a36f64aaa5e87d
rnalstmd <msku1896@gmail.com>
2024-12-11 23:50:48 +0900

    Update main.ts
    - refresh iframe when toggled
    - if there is no iframe, error message appears
    - if there is more than one iframe, prompt appears
    - ribbon
    - command pallete can be also used

--- main.ts
+++ main.ts
@@ -1,134 +1,84 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, WorkspaceLeaf, Notice } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class IframeReloaderPlugin extends Plugin {
+  async onload() {
+    console.log("Iframe Reloader Plugin loaded");
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    // 리본 아이콘 추가
+    this.addRibbonIcon("refresh-cw", "Reload iframe", () => {
+      this.reloadIframe();
+    });
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    // 명령어 추가
+    this.addCommand({
+      id: "reload-iframe",
+      name: "Reload iframe in the current note",
+      callback: () => this.reloadIframe(),
+    });
+  }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  reloadIframe() {
+    const leaf: WorkspaceLeaf = this.app.workspace.activeLeaf;
 
-	async onload() {
-		await this.loadSettings();
+    if (!leaf) {
+      new Notice("No active note found.");
+      return;
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // 모든 iframe 요소 가져오기
+    const iframes = document.querySelectorAll("iframe");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    if (iframes.length === 0) {
+      new Notice("No iframe found in the current note.");
+      return;
+    }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    if (iframes.length === 1) {
+      // iframe이 하나인 경우 바로 리로드
+      const iframe = iframes[0];
+      const src = iframe.getAttribute("src");
+      if (src) {
+        iframe.setAttribute("src", src);
+        new Notice("Iframe reloaded successfully.");
+      } else {
+        new Notice("Iframe found but no src attribute present.");
+      }
+      return;
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    // iframe이 여러 개인 경우 사용자 선택
+    const options = Array.from(iframes).map((iframe, index) => {
+      const src = iframe.getAttribute("src") || "No src";
+      return `Iframe ${index + 1}: ${src}`;
+    });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    const userChoice = prompt(
+      `Select an iframe to reload:\n\n${options.join("\n")}\n\nEnter the number (1-${iframes.length}):`
+    );
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    if (!userChoice) {
+      new Notice("Iframe reload canceled.");
+      return;
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    const selectedIndex = parseInt(userChoice, 10) - 1;
+    if (isNaN(selectedIndex) || selectedIndex < 0 || selectedIndex >= iframes.length) {
+      new Notice("Invalid selection.");
+      return;
+    }
 
-	onunload() {
+    // 선택된 iframe 리로드
+    const selectedIframe = iframes[selectedIndex];
+    const selectedSrc = selectedIframe.getAttribute("src");
+    if (selectedSrc) {
+      selectedIframe.setAttribute("src", selectedSrc);
+      new Notice(`Iframe ${selectedIndex + 1} reloaded successfully.`);
+    } else {
+      new Notice(`Iframe ${selectedIndex + 1} has no src attribute.`);
+    }
+  }
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  onunload() {
+    console.log("Iframe Reloader Plugin unloaded");
+  }
 }
